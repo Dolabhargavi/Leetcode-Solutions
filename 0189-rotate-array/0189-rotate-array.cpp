9e8f7ec60a05deb99c0f273a79e0ecdc7f423d06
Dolabhargavi <101411367+Dolabhargavi@users.noreply.github.com>
2022-12-10 12:27:22 +0530

    Update 0189-rotate-array.cpp

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,6 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
       int n=nums.size();
       k=k%n;
+       ''' use emplace_back() to push elements then use erase func to remove extra elements'''
       reverse(nums.begin(),nums.end());
       reverse(nums.begin(),nums.begin()+k);
       reverse(nums.begin()+k,nums.end());
