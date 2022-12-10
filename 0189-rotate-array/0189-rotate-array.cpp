fc712f7caef5436853590286c7622dac94b6dc2d
Dolabhargavi <101411367+Dolabhargavi@users.noreply.github.com>
2022-12-10 12:25:16 +0530

    Time: 39 ms (80.83%), Space: 25 MB (57.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,13 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
       int n=nums.size();
       k=k%n;
-      k=n-k;
-     for(int i=0;i<k;i++)
-      {
-          nums.emplace_back(nums[i]);
-      }
-        nums.erase(nums.begin(),nums.begin()+k);
-
+      reverse(nums.begin(),nums.end());
+      reverse(nums.begin(),nums.begin()+k);
+      reverse(nums.begin()+k,nums.end());
       
     }
 };
