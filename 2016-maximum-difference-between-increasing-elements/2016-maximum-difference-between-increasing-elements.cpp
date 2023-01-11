be6d0a6ff6f6db5e926193d930a8175753bc7bcd
Bhargavi Dola <101411367+Dolabhargavi@users.noreply.github.com>
2023-01-11 16:44:17 +0530

    Time: 4 ms (82.97%), Space: 8.4 MB (42.79%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -1,17 +1,31 @@
+// class Solution {
+// public:
+//     int maximumDifference(vector<int>& nums) {
+//        int ans=0,i;
+//         for(i=0;i<nums.size()-1;i++)
+//         {
+//            for(int j=i+1;j<nums.size();j++)
+//            {
+//                ans=max(ans,nums[j]-nums[i]);       //order(n^2)
+//            }
+//         } 
+//         ans=max(ans,nums[i]-nums[0]); 
+//         if(ans==0)
+//             return -1;
+//         return ans;
+//     }
+// };
 class Solution {
-public:
+    public:
     int maximumDifference(vector<int>& nums) {
-       int ans=0,i;
-        for(i=0;i<nums.size()-1;i++)
+        int ans=-1,min=nums[0];
+        for(int i=1;i<nums.size();i++)
         {
-           for(int j=i+1;j<nums.size();j++)
-           {
-               ans=max(ans,nums[j]-nums[i]);
-           }
-        } 
-        ans=max(ans,nums[i]-nums[0]); 
-        if(ans==0)
-            return -1;
+            if(nums[i]>min) 
+                ans=max(ans,nums[i]-min);  //order(n)
+            else 
+                min=nums[i];
+        }
         return ans;
     }
 };
