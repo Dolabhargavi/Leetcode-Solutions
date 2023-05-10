d7d05d4029a3603645c911e01769949a5b926d99
Bhargavi Dola <101411367+Dolabhargavi@users.noreply.github.com>
2023-05-10 14:54:29 +0530

    Time: 7 ms (39.85%), Space: 10.6 MB (96.83%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,29 +1,60 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-    
-      ListNode *p = head, *q = head;
-        while (n--) q = q->next;
-        if (!q) return head->next;
-        while (q->next) {
-            p = p->next;
-            q = q->next;
+class Solution
+{
+    public:
+        ListNode* removeNthFromEnd(ListNode *head, int n)
+        {
+
+           	// ListNode *p = head, *q = head;
+           	//   while (n--) q = q->next;
+           	//   if (!q) return head->next;
+           	//   while (q->next) {
+           	//       p = p->next;
+           	//       q = q->next;
+           	//   }
+           	//   ListNode* toDelete = p->next;
+           	//   p->next = p->next->next;
+           	//   delete toDelete;
+           	//   return head; 
+           	//   return head;
+
+            if (head->next == NULL && n == 1)
+            {
+                head = NULL;
+                return head;
+            }
+
+            ListNode *curr = head;
+            int cnt = 1;
+            while (curr->next != NULL)
+            {
+                curr = curr->next;
+                cnt++;
+            }
+            cnt = cnt - n;
+            cout << cnt;
+            if (cnt == 0)
+            {
+                head = head->next;
+                return head;
+            }
+            curr = head;
+            while (--cnt)
+            {
+                curr = curr->next;
+            }
+            if (curr->next->next == NULL)
+                curr->next = NULL;
+            else curr->next = curr->next->next;
+            return head;
         }
-        ListNode* toDelete = p->next;
-        p->next = p->next->next;
-        delete toDelete;
-        return head; 
-        return head;
-        
-    }
 };
