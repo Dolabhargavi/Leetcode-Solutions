a8c608517db553d8b690c00485f72d16c26f64dc
Bhargavi Dola <101411367+Dolabhargavi@users.noreply.github.com>
2023-09-07 12:13:23 +0530

    Time: 3400 ms (30.14%), Space: 0B (100.00%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,13 @@
+
+# select customer_id,count(visit_id) as count_no_trans
+# from visits
+# where visit_id not in (select distinct visit_id from transactions) group by customer_id;
+
+# using joins
+
+select v.customer_id, count(v.visit_id)as count_no_trans
+from visits v
+left join transactions t
+on v.visit_id=t.visit_id
+where t.transaction_id is null
+group by v.customer_id;
