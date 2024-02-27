6d071f199f4973cff1ba0adf0ebccad18d56d063
Bhargavi Dola <101411367+Dolabhargavi@users.noreply.github.com>
2024-02-27 14:22:37 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (15.28%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,19 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// void preorder(TreeNode* root,vector<int>&v)
+//     {
+//         if(root==NULL)
+//             return;
+//         v.push_back(root->val);
+//         preorder(root->left,v);
+//         preorder(root->right,v);
+//     }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//        vector<int> v;
+//         preorder(root,v);
+//         return v;
+//     }
+
+
 class Solution {
 public:
-    void preorder(TreeNode* root,vector<int>&v)
-    {
-        if(root==NULL)
-            return;
-        v.push_back(root->val);
-        preorder(root->left,v);
-        preorder(root->right,v);
-    }
+    
     vector<int> preorderTraversal(TreeNode* root) {
-       vector<int> v;
-        preorder(root,v);
-        return v;
+       vector<int> preorder;
+        stack<TreeNode *>st;
+        if(root==NULL) return preorder;
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode *node=st.top();
+            st.pop();
+            preorder.emplace_back(node->val);
+            if(node->right!=NULL) st.push(node->right);
+            if(node->left!=NULL) st.push(node->left);
+        }
+        return preorder;
     }
 };
+
