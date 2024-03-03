10f6bc4dbba9f1405419d87cd762ec750c627bd3
Bhargavi Dola <101411367+Dolabhargavi@users.noreply.github.com>
2024-03-03 20:44:09 +0530

    Time: 3 ms (64.94%), Space: 13.2 MB (45.90%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -14,47 +14,16 @@ class Solution
         ListNode* removeNthFromEnd(ListNode *head, int n)
         {
 
-           	// ListNode *p = head, *q = head;
-           	//   while (n--) q = q->next;
-           	//   if (!q) return head->next;
-           	//   while (q->next) {
-           	//       p = p->next;
-           	//       q = q->next;
-           	//   }
-           	//   ListNode* toDelete = p->next;
-           	//   p->next = p->next->next;
-           	//   delete toDelete;
-           	//   return head; 
-           	//   return head;
+           	ListNode *p = head, *q = head;
+           	  while (n--) q = q->next;
+           	  if (!q) return head->next;
+           	  while (q->next) {
+           	      p = p->next;
+           	      q = q->next;
+           	  }
+           	  p->next = p->next->next;
+           	  return head; 
 
-            if (head->next == NULL && n == 1)
-            {
-                head = NULL;
-                return head;
-            }
-
-            ListNode *curr = head;
-            int cnt = 1;
-            while (curr->next != NULL)
-            {
-                curr = curr->next;
-                cnt++;
-            }
-            cnt = cnt - n;
-            cout << cnt;
-            if (cnt == 0)
-            {
-                head = head->next;
-                return head;
-            }
-            curr = head;
-            while (--cnt)
-            {
-                curr = curr->next;
-            }
-            if (curr->next->next == NULL)
-                curr->next = NULL;
-            else curr->next = curr->next->next;
-            return head;
+            
         }
 };
