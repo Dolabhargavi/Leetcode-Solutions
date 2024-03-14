c91c720a83e72db7f08299f46aa0620a8d40804f
Bhargavi Dola <101411367+Dolabhargavi@users.noreply.github.com>
2024-03-14 21:34:05 +0530

    Time: 49 ms (30.91%), Space: 38.4 MB (20.69%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,23 +1,41 @@
+// class Solution {
+// public:
+//     int numSubarraysWithSum(vector<int>& nums, int goal) {
+//         int ans=0;
+//         int sum=0;
+//         vector<int>vec;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//           sum+=nums[i];
+//           vec.emplace_back(sum);
+//           if(sum==goal) ans++;
+//         }
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             for(int j=0;j<i;j++)
+//             {
+//                 if((vec[i]-vec[j])== goal) ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int ans=0;
-        int sum=0;
-        vector<int>vec;
-        for(int i=0;i<nums.size();i++)
-        {
-          sum+=nums[i];
-          vec.emplace_back(sum);
-          if(sum==goal) ans++;
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                if((vec[i]-vec[j])== goal) ans++;
+        unordered_map<int, int> m;
+        int sum = 0;
+        m[0] = 1;
+        int cnt = 0;
+        for (int num : nums) {
+            sum += num;
+            if (m.find(sum - goal) != m.end()) {
+                cnt += m[sum - goal];
             }
-        }
-         
-        return ans;
+            m[sum]++;
+        }   
+        return cnt;
     }
 };
