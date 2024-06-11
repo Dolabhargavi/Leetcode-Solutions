5ca0dfd2a26ff96379970bf873fc943c92a5ca71
Bhargavi Dola <101411367+Dolabhargavi@users.noreply.github.com>
2024-06-11 20:06:06 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (41.93%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,31 +1,30 @@
-class Solution {
-public:
-    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-       unordered_map<int,int>map;
-        vector<int>ans;
-        vector<int>tmp;
-        for(int i=0;i<arr1.size();i++)
+class Solution
+{
+    public:
+        vector<int> relativeSortArray(vector<int> &arr1, vector<int> &arr2)
         {
-               map[arr1[i]]++;
-        }
-        for(int i=0;i<arr2.size();i++)
-        {
-            while(map[arr2[i]]>0)
+            map<int, int> map;
+            vector<int> ans;
+            for (int i = 0; i < arr1.size(); i++)
             {
-                ans.emplace_back(arr2[i]);
-                map[arr2[i]]--;
+                map[arr1[i]]++;
             }
+            for (int i = 0; i < arr2.size(); i++)
+            {
+                while (map[arr2[i]] > 0)
+                {
+                    ans.emplace_back(arr2[i]);
+                    map[arr2[i]]--;
+                }
+            }
+            for (auto it: map)
+            {
+                while (it.second)
+                {
+                    ans.push_back(it.first);
+                    it.second--;
+                }
+            }
+            return ans;
         }
-        for(int i=0;i<arr1.size();i++)
-        {
-               if(map[arr1[i]]>0)
-                   tmp.emplace_back(arr1[i]);
-        }
-        sort(tmp.begin(),tmp.end());
-        for(int i=0;i<tmp.size();i++)
-        {
-            ans.emplace_back(tmp[i]);
-        }
-        return ans;
-    }
 };
